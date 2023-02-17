32718df4f14b157c7db36c9588108c3ec30c5100
tk <kkom_bom@naver.com>
2023-02-17 17:31:31 +0900

    2023/02/17

---
+++ silhyun/src/main/java/co/yedam/silhyun/SilhyunApplication.java
@@ -0,0 +1,15 @@
+package co.yedam.silhyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SilhyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilhyunApplication.class, args);
+	}
+
+
+}
