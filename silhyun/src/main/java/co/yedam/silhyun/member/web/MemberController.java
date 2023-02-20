674df084e411a2a363f70b5f9290b6ea8faabc15
sssohae310 <sssohae310@gmail.com>
2023-02-20 17:01:13 +0900

    add

--- silhyun/src/main/java/co/yedam/silhyun/member/web/MemberController.java
+++ silhyun/src/main/java/co/yedam/silhyun/member/web/MemberController.java
@@ -9,9 +9,44 @@ public class MemberController {
 		
 	@Autowired
 	
-	@GetMapping("/memberList")
-	public String memberList() {
-		return "/admin/memberList";
+	@GetMapping("/eventManage")
+	public String eventManage() {
+		return "/admin/eventManage";
+	}
+	
+	@GetMapping("/memberAccept")
+	public String memberAccept() {
+		return "/admin/memberAccept";
+	}
+	
+	@GetMapping("/memberManage")
+	public String memberManage() {
+		return "/admin/memberManage";
+	}
+	
+	@GetMapping("/orderManage")
+	public String orderManage() {
+		return "/admin/orderManage";
+	}
+	
+	@GetMapping("/qstManage")
+	public String qstManage() {
+		return "/admin/qstManage";
+	}
+	
+	@GetMapping("/quitManage")
+	public String quitManage() {
+		return "/admin/quitManage";
+	}
+	
+	@GetMapping("/rankManage")
+	public String rankManage() {
+		return "/admin/rankManage";
+	}
+	
+	@GetMapping("/reportManagr")
+	public String reportManagr() {
+		return "/admin/reportManagr";
 	}
 	
 
