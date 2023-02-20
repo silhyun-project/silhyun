40bf0c271a84f4cbf3c98cd09f05718debeac619
sssohae310 <sssohae310@gmail.com>
2023-02-20 15:39:24 +0900

    Merge branch 'main' of https://github.com/silhyun-project/silhyun.git into sohae

--- silhyun/src/main/java/co/yedam/silhyun/SilhyunApplication.java
+++ silhyun/src/main/java/co/yedam/silhyun/SilhyunApplication.java
@@ -2,7 +2,6 @@ package co.yedam.silhyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class SilhyunApplication {
