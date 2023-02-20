5961be13108edd22b109f67937bee5bd9739c654
sssohae310 <sssohae310@gmail.com>
2023-02-20 16:32:55 +0900

    add viewPages

--- silhyun/src/main/java/co/yedam/silhyun/member/web/MemberController.java
+++ silhyun/src/main/java/co/yedam/silhyun/member/web/MemberController.java
@@ -1,10 +1,18 @@
 package co.yedam.silhyun.member.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
 		
+	@Autowired
+	
+	@GetMapping("/memberList")
+	public String memberList() {
+		return "/admin/memberList";
+	}
 	
 
 	
