ac1504ddb78794501f3e53d6166bb8a099e0d23a
lim <cup1022@naver.com>
2023-02-21 17:24:10 +0900

    Merge branch 'main' of https://github.com/silhyun-project/silhyun.git into hyelim

--- silhyun/src/main/java/co/yedam/silhyun/mypage/web/mypageAoController.java
+++ silhyun/src/main/java/co/yedam/silhyun/mypage/web/mypageAoController.java
@@ -1,17 +1,30 @@
 package co.yedam.silhyun.mypage.web;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequestMapping("/modPfAo")
+@Controller
 public class mypageAoController {
 	
-	@PostMapping()
+	@GetMapping("/mypageAo")
+	public String mypageAo() {
+		return "mypageAo/mypageAo";
+	}
+	
+	@GetMapping("/modPfAo")
 	public String modpfAo() {
 			
-			return "/mypageAo/modPfAo";
+			return "mypageAo/modPfAo";
 		}
+	@GetMapping("/resManage")
+	public String resManage() {
+		
+		return "mypageAo/resManage";
+	}
+	@GetMapping("/classManage")
+	public String classManage() {
+		
+		return "mypageAo/classManage";
+	}
 
 }
