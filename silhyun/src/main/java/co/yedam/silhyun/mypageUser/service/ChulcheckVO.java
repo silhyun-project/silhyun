5d214927ccddb10020690144ed2eed2f20ddc7df
Junwoo Park <wnsdn0626@naver.com>
2023-02-21 10:03:26 +0900

    2023.02.20 vo

---
+++ silhyun/src/main/java/co/yedam/silhyun/mypageUser/service/ChulcheckVO.java
@@ -0,0 +1,13 @@
+package co.yedam.silhyun.mypageUser.service;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ChulcheckVO {
+
+	Date checkDate;
+	String id;
+	
+}
---
+++ silhyun/src/main/java/co/yedam/silhyun/mypageUser/service/PointVO.java
@@ -0,0 +1,16 @@
+package co.yedam.silhyun.mypageUser.service;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class PointVO {
+
+	int poNum;
+	String id;
+	int savePo;
+	Date saveDate;
+	int saveNum;
+	
+}
