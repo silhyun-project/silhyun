20de11cd7a3edee4470cd4a7089f5bda75e66943
lwedunbloomd <kkah0210@naver.com>
2023-02-24 17:29:04 +0900

    1

--- silhyun/src/main/java/co/yedam/silhyun/portfolio/service/AroundService.java
+++ silhyun/src/main/java/co/yedam/silhyun/portfolio/service/AroundService.java
@@ -6,5 +6,6 @@ import co.yedam.silhyun.portfolio.vo.PortfolioVO;
 
 public interface AroundService {
 
-	List<PortfolioVO> getAroundList();
+	List<PortfolioVO> getAroundList();	//둘러보기 들어가면 보이는 어쩌구
+	List<PortfolioVO> AroundTagList();	//태그별로 보이기
 }
