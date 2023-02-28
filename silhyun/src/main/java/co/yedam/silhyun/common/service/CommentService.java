11058f9fb8c1694d77933906f5189283397298bf
kkrubbingbbong <wnsdn0626@naver.com>
2023-02-28 17:34:56 +0900

    ,.

---
+++ silhyun/src/main/java/co/yedam/silhyun/common/service/CommentService.java
@@ -0,0 +1,10 @@
+package co.yedam.silhyun.common.service;
+
+import java.util.List;
+
+import co.yedam.silhyun.common.vo.CommentVO;
+
+public interface CommentService {
+	List<CommentVO> CommentList();
+	
+}
