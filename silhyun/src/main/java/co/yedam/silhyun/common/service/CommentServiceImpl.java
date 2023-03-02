9c1c5584e9333058d92f84bc491c659946130408
kkrubbingbbong <wnsdn0626@naver.com>
2023-03-02 17:46:24 +0900

    230302

--- silhyun/src/main/java/co/yedam/silhyun/common/service/CommentServiceImpl.java
+++ silhyun/src/main/java/co/yedam/silhyun/common/service/CommentServiceImpl.java
@@ -17,4 +17,46 @@ public class CommentServiceImpl implements CommentService{
 		// TODO Auto-generated method stub
 		return mapper.getCommentList();
 	}
+
+	@Override
+	public List<CommentVO> getReplyList() {
+		// TODO Auto-generated method stub
+		return mapper.getReplyList();
+	}
+
+	@Override
+	public int commentInsert(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.commentInsert(vo);
+	}
+
+	@Override
+	public int commentDelete(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.commentDelete(vo);
+	}
+
+	@Override
+	public int commentUpdate(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.commentUpdate(vo);
+	}
+
+	@Override
+	public int replyInsert(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.replyInsert(vo);
+	}
+
+	@Override
+	public int replyDelete(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.replyDelete(vo);
+	}
+
+	@Override
+	public int replyUpdate(CommentVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.replyUpdate(vo);
+	}
 }
--- silhyun/src/main/java/co/yedam/silhyun/common/web/CommentController.java
+++ silhyun/src/main/java/co/yedam/silhyun/common/web/CommentController.java
@@ -1,13 +1,13 @@
 package co.yedam.silhyun.common.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import co.yedam.silhyun.common.service.CommentService;
+import co.yedam.silhyun.common.vo.CommentVO;
 
 @Controller
 public class CommentController {
@@ -15,11 +15,49 @@ public class CommentController {
 	@Autowired
 	private CommentService commentService;
 	
-	@RequestMapping("/silhyun/commentReply")
-	   public String commentReply(Model model) {
-	      model.addAttribute("commentList", commentService.getCommentList());
+	@GetMapping("/silhyun/commentReply")
+	public String commentReply() {
 	      
-	      return "mypageUser/commentReply";
+		return "mypageUser/commentReply";
 	   }
 	
+	@GetMapping("/commentList")
+	public String commentList(Model model) {
+		model.addAttribute("commentList", commentService.getCommentList());
+		
+		return "mypageUser/commentReply";
+	}
+	
+//	@GetMapping("/replyList")
+//	   public String replyList(Model model) {
+//	      model.addAttribute("replyList", commentService.getReplyList());
+//	      
+//	      return "mypageUser/commentReply";
+//	   }
+//	
+//	@PostMapping("/commentInsert")
+//	public String commentInsert(CommentVO vo) {
+//		
+//		
+//		return "mypageUser/commentReply";
+//	}
+//	
+//	@PostMapping("/commentDelete")
+//	public String commentDelete(CommentVO vo) {
+//		
+//		
+//		return "mypageUser/commentReply";
+//	}
+//	@PostMapping("/commentUpdate")
+//	public String commentUpdate(CommentVO vo) {
+//	
+//	
+//		return "mypageUser/commentReply";
+//	}
+//	@PostMapping("/replyInsert")
+//	public String replyInsert(CommentVO vo) {
+//	
+//		return "mypageUser/commentReply";
+//	}
+//	
 }
