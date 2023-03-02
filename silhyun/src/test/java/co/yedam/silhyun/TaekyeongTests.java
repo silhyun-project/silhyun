20a0a7fd5138c5516631ddaf90ae83a6b19a255e
tk <kkom_bom@naver.com>
2023-03-02 17:47:34 +0900

    ..

--- silhyun/src/test/java/co/yedam/silhyun/TaekyeongTests.java
+++ silhyun/src/test/java/co/yedam/silhyun/TaekyeongTests.java
@@ -13,19 +13,19 @@ public class TaekyeongTests {
 	@Autowired PageTestService service;
 
 	//@Test
-	public void 페이징검색테스트() {
-		Criteria cri = new Criteria();
-		cri.setKeyword("리뷰");
-		cri.setType("C");
-		PageVO vo = new PageVO(100, 10, cri);
-	}
-	
-	@Test
-	public void 총갯수() {
-		Criteria cri = new Criteria();
-		cri.setKeyword("리뷰");	
-		cri.setType("CW");
-		service.getTotalCount(cri);
-		System.out.println("총갯수는?"  +service.getTotalCount(cri));
-	}
+//	public void 페이징검색테스트() {
+//		Criteria cri = new Criteria();
+//		cri.setKeyword("리뷰");
+//		cri.setType("C");
+//		PageVO vo = new PageVO(100, 10, cri);
+//	}
+//	
+//	@Test
+//	public void 총갯수() {
+//		Criteria cri = new Criteria();
+//		cri.setKeyword("리뷰");	
+//		cri.setType("CW");
+//		service.getTotalCount(cri);
+//		System.out.println("총갯수는?"  +service.getTotalCount(cri));
+//	}
 }
