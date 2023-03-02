8b52659a5bf5de0d3a040e4ff88a998e15d7dbc5
lim <cup1022@naver.com>
2023-03-02 17:36:56 +0900

    0302

--- silhyun/src/main/java/co/yedam/silhyun/mypage/service/MypageAoServiceImpl.java
+++ silhyun/src/main/java/co/yedam/silhyun/mypage/service/MypageAoServiceImpl.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.yedam.silhyun.event.vo.CouponVO;
+import co.yedam.silhyun.event.vo.EventVO;
+import co.yedam.silhyun.member.vo.MemberVO;
+import co.yedam.silhyun.member.vo.PhotographerVO;
 import co.yedam.silhyun.mypage.map.MypageAoMapper;
 import co.yedam.silhyun.order.vo.ReserVO;
 
@@ -18,5 +22,28 @@ public class MypageAoServiceImpl implements MypageAoService {
 	public List<ReserVO> getReserList(){
 		
 		return mypageAoMapper.getReserList();
+	}
+	
+	@Override
+	public List<MemberVO> getPhotoinfo(){
+		
+		return mypageAoMapper.getPhotoinfo();
+	}
+
+	@Override
+	public int ptgInfoUpdate(PhotographerVO vo) {
+	
+		return mypageAoMapper.ptgInfoUpdate(vo);
+	}
+	@Override
+	public int applyEvent(EventVO vo) {
+		
+		return mypageAoMapper.applyEvent(vo);
+	}
+	
+	@Override
+	public int applyECoupon(CouponVO vo) {
+		
+		return mypageAoMapper.applyECoupon(vo);
 	}
 }
