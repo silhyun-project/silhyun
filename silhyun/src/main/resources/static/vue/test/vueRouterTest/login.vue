9bbc7e783547daa5567107af71e0faf594bd7779
tk <kkom_bom@naver.com>
2023-03-03 17:00:03 +0900

    s

---
+++ silhyun/src/main/resources/static/vue/test/vueRouterTest/login.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <form><input><input><button>login</button>
+    <button type="button" @click="gopage">회원가입</button>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    },
+    methods: {
+        gopage : function(){
+            this.$router.push('/signin')
+        },
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
