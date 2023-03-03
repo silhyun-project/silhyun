a3f4d80151f8ebefb922d0b2f3b245bb82fbbe84
lim <cup1022@naver.com>
2023-03-03 17:28:30 +0900

    0303

--- silhyun/src/main/java/co/yedam/silhyun/event/vo/EventVO.java
+++ silhyun/src/main/java/co/yedam/silhyun/event/vo/EventVO.java
@@ -3,6 +3,7 @@ package co.yedam.silhyun.event.vo;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -11,7 +12,7 @@ import lombok.Data;
 @Data
 public class EventVO {
 	private String eventNum;
-	private String id;
+	private String ptgId;
 	private String name;
 	private int term;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -20,7 +21,6 @@ public class EventVO {
 	private String cntn;
 	private String thni;
 	private String bnph;
-	private String bnphSta;
 	private String cfmCd;
 	private String cpnPubSta;
 	private String regionCd;
