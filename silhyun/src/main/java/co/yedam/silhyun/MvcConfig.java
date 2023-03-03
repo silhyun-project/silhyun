c43633ec68b3a5b8981da0e34b4a37558a8a2816
tk <kkom_bom@naver.com>
2023-03-03 16:59:55 +0900

    Merge branch 'main' of https://github.com/silhyun-project/silhyun.git into taekyeong

--- silhyun/src/main/java/co/yedam/silhyun/MvcConfig.java
+++ silhyun/src/main/java/co/yedam/silhyun/MvcConfig.java
@@ -1,5 +1,6 @@
 package co.yedam.silhyun;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/home").setViewName("home/home");
+		registry.addViewController("/").setViewName("home/home");
 		registry.addViewController("/login").setViewName("login");
 	}
+	
 
 }
--- silhyun/src/main/java/co/yedam/silhyun/member/service/MemberServiceImpl.java
+++ silhyun/src/main/java/co/yedam/silhyun/member/service/MemberServiceImpl.java
@@ -1,5 +1,21 @@
 package co.yedam.silhyun.member.service;
 
-public class MemberServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.yedam.silhyun.member.map.MemberMapper;
+import co.yedam.silhyun.member.vo.MemberVO;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+	
+	@Autowired MemberMapper map;
+
+	@Override
+	public MemberVO memeberSelect(MemberVO vo) {
+		// TODO Auto-generated method stub
+		return map.memeberSelect(vo);
+	}
+	
 
 }
