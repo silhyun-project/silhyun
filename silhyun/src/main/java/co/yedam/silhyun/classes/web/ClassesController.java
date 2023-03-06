572e8705bffb21ca9c6c1bf8036f683e7203ae98
sssohae310 <sssohae310@gmail.com>
2023-03-06 18:28:57 +0900

    0306

--- silhyun/src/main/java/co/yedam/silhyun/classes/web/ClassesController.java
+++ silhyun/src/main/java/co/yedam/silhyun/classes/web/ClassesController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.yedam.silhyun.classes.service.ClassesService;
+import co.yedam.silhyun.classes.vo.InetClassesWtchVO;
 
 @Controller
 public class ClassesController {
@@ -24,9 +27,12 @@ public class ClassesController {
 		return "/classes/classesMain";
 	}
 	
-	@GetMapping("/classesVideo")
-	public String classesVideo() {
-		return "/classes/classesVideo";
+	@RequestMapping("/classes/classesVideo")
+	public String classesVideo(Model model, @RequestParam("inetNum") String inetNum) {
+	    System.out.println("inetNum="+inetNum);
+	    model.addAttribute("IV", ClassesService.selectIV("2", inetNum, "catLove"));
+	    System.out.println("컨트롤러에 온 비디오의 세부 IVModel"+model);
+	    return "/classes/classesVideo";
 	}
 	
 	@GetMapping("/myClasses")
@@ -37,6 +43,7 @@ public class ClassesController {
 	@RequestMapping("classes/myClassVideos")
 	public String myClassesVidios(Model model) {
 		model.addAttribute("IVInfo",ClassesService.getClassIVInfo("2", "catLove"));
+		
 		System.out.println("컨트롤러로 온 클래스IVInfo"+ model);
 		return "/classes/myClassesVideos";	
 	}
@@ -55,5 +62,26 @@ public class ClassesController {
 	public String eventClasses() {
 		return "/classes/eventClasses";
 	}
-
+	
+	
+    @RequestMapping("/insertWInfo")
+    @ResponseBody
+    public String insertWInfo(@RequestParam("inetNum") String inetNum,
+                              @RequestParam("classNum") String classNum,
+                              @RequestParam("id") String id,
+                              @RequestParam("stwTime") String stwTime,
+                              @RequestParam("ewTime") String ewTime,
+                              @RequestParam("cumlwTime") String cumlwTime) {
+        InetClassesWtchVO ICVo = new InetClassesWtchVO();
+        ICVo.setInetNum(inetNum);
+        ICVo.setClassNum(classNum);
+        ICVo.setId(id);
+        ICVo.setStwTime(stwTime);
+        ICVo.setEwTime(ewTime);
+        ICVo.setCumlwTime(cumlwTime);
+        // service 메소드를 호출하여 데이터 삽입 또는 업데이트 수행
+        ClassesService.insertWInfo(ICVo);
+        return "success";
+    }
+	
 }
