2b690d5772df691b71592f1e75c3a86e681a31c2
kkrubbingbbong <wnsdn0626@naver.com>
2023-03-07 17:30:22 +0900

    .,

--- silhyun/src/main/java/co/yedam/silhyun/CustomErrorController.java
+++ silhyun/src/main/java/co/yedam/silhyun/CustomErrorController.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
-    // error path를 꼭 "/error" 로 하자!
-	private final String ERROR_PATH = "/error";
-
-    @GetMapping(ERROR_PATH)
-    public String redirectRoot(){
-        return "home/home";
-    }
-
-    public String getErrorPath(){
-        return null;
-    }
+//    // error path를 꼭 "/error" 로 하자!
+//	private final String ERROR_PATH = "/error";
+//
+//    @GetMapping(ERROR_PATH)
+//    public String redirectRoot(){
+//        return "home/home";
+//    }
+//
+//    public String getErrorPath(){
+//        return null;
+//    }
 }
