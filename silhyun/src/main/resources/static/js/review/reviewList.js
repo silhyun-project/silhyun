622648cd8a202121b3853196ea6b6ba379e2a485
tk <kkom_bom@naver.com>
2023-03-13 19:11:50 +0900

    흑흑

--- silhyun/src/main/resources/static/js/review/reviewList.js
+++ silhyun/src/main/resources/static/js/review/reviewList.js
@@ -69,8 +69,8 @@
     	console.log($("#searchFrm").find("input[name='pageNum']").val())
     	
     	
-    	//ajaxReiew($('#searchFrm').serialize()) //form안에있는 name이랑 value값을 들고온다.
-    	searchFrm.submit(); //폼태그값을 스트링으로 
+    	ajaxReiew($('#searchFrm').serialize()) //form안에있는 name이랑 value값을 들고온다.
+    	//searchFrm.submit(); //폼태그값을 스트링으로 
     })
     
 
