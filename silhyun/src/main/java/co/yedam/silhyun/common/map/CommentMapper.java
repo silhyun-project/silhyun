771775526e508343c6ef806c0248fab4dda3bee1
lwedunbloomd <kkah0210@naver.com>
2023-03-14 17:47:37 +0900

    Merge branch 'main' of https://github.com/silhyun-project/silhyun.git into kyeong_a

--- silhyun/src/main/java/co/yedam/silhyun/common/map/CommentMapper.java
+++ silhyun/src/main/java/co/yedam/silhyun/common/map/CommentMapper.java
@@ -6,18 +6,18 @@ import co.yedam.silhyun.common.vo.CommentVO;
 
 
 public interface CommentMapper {
-	List<CommentVO> getCommentList(); // 댓글 리스트
+	List<CommentVO> getCommentList(String portNum); // 댓글 리스트
 	
 	int commentInsert(CommentVO vo); // 댓글 입력
 	
 	int commentDelete(CommentVO vo); // 댓글 삭제
 	
-	int commentUpdate(CommentVO vo); // 댓글 수정
+
 	
-	int replyInsert(CommentVO vo); // 대댓글 입력
+
 	
 	int replyDelete(CommentVO vo); // 대댓글 삭제
 	
-	int replyUpdate(CommentVO vo); // 대댓글 수정
+	
 	
 }
